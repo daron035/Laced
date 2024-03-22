fc89f7a367f6763b343ede7043cb38917030ccb1
daron035 <kamil249@mail.ru>
2024-03-22 16:22:24 +0500

    add media

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -12,6 +12,8 @@ do
     pwd
 done
 
+./manage.py makemigrations
+./manage.py migrate --noinput
 ./manage.py collectstatic --noinput
 
 gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
